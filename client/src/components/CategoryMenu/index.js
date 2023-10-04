7a35723362dab1f55959423985d9464419eba5b3
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-04 01:02:32 -0400

    fixed cart so it shows up in corner and changed the choose a category styling around

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,17 +1,23 @@
 import React, { useEffect } from "react";
+
 import { useQuery } from "@apollo/client";
+
 import { useDispatch, useSelector } from "react-redux";
+
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
 } from "../../utils/actions";
+
 import { QUERY_CATEGORIES } from "../../utils/queries";
+
 import { idbPromise } from "../../utils/helpers";
 
+// import "./CategoryMenu.css"; // Import your CSS file
+
 function CategoryMenu() {
-  const state = useSelector((state) => {
-    return state;
-  });
+  const state = useSelector((state) => state);
+
   const dispatch = useDispatch();
 
   const { categories } = state;
@@ -22,8 +28,10 @@ function CategoryMenu() {
     if (categoryData) {
       dispatch({
         type: UPDATE_CATEGORIES,
+
         categories: categoryData.categories,
       });
+
       categoryData.categories.forEach((category) => {
         idbPromise("categories", "put", category);
       });
@@ -31,6 +39,7 @@ function CategoryMenu() {
       idbPromise("categories", "get").then((categories) => {
         dispatch({
           type: UPDATE_CATEGORIES,
+
           categories: categories,
         });
       });
@@ -40,23 +49,28 @@ function CategoryMenu() {
   const handleClick = (id) => {
     dispatch({
       type: UPDATE_CURRENT_CATEGORY,
+
       currentCategory: id,
     });
   };
 
   return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            handleClick(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
+    <div className="category-menu">
+      <h2 className="category-menu__title">Choose a Category</h2>
+
+      <div className="category-menu__buttons">
+        {categories.map((item) => (
+          <button
+            key={item._id}
+            onClick={() => {
+              handleClick(item._id);
+            }}
+            className="category-menu__button"
+          >
+            {item.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+// import "./Nav.module.css"; // Import your CSS file
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+            <Link to="/orderHistory">Order History</Link>
           </li>
           <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
@@ -25,32 +22,26 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       );
     }
   }
-
   return (
     <header className="flex-row px-1">
       <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+        <Link to="/" className="brand">
+          <span role="img" aria-label="shopping bag">
+            🛍️
+          </span>
+          The-Shop
         </Link>
       </h1>
-
-      <nav>
-        {showNavigation()}
-      </nav>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
