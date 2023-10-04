96ca37330209879d0057a7ba5a01bd4bcbde0f74
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-03 23:54:35 -0400

    refactored login.js and created Login.modules.css to keep css file to the login page only

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
+import Swal from "sweetalert2";
+import styles from "./Login.module.css"; // Import the CSS module
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error }] = useMutation(LOGIN);
-
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -16,11 +17,21 @@ function Login(props) {
       });
       const token = mutationResponse.data.login.token;
       Auth.login(token);
+      Swal.fire({
+        icon: "success",
+        title: "Login successful!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
     } catch (e) {
+      Swal.fire({
+        icon: "error",
+        title: "Login failed",
+        text: "The provided credentials are incorrect",
+      });
       console.log(e);
     }
   };
-
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
@@ -28,12 +39,12 @@ function Login(props) {
       [name]: value,
     });
   };
-
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
-
-      <h2>Login</h2>
+    <div className={styles.container}>
+      <Link to="/signup" className={styles.link}>
+        ← Go to Signup
+      </Link>
+      <h2 className={styles.heading}>Login</h2>
       <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
           <label htmlFor="email">Email address:</label>
@@ -46,24 +57,26 @@ function Login(props) {
           />
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+          <label htmlFor="password">Password:</label>
           <input
             placeholder="******"
             name="password"
             type="password"
-            id="pwd"
+            id="password"
             onChange={handleChange}
           />
         </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
         <div className="flex-row flex-end">
           <button type="submit">Submit</button>
         </div>
       </form>
+      {error ? (
+        <div className={styles["error-container"]}>
+          <p className={styles["error-text"]}>
+            The provided credentials are incorrect
+          </p>
+        </div>
+      ) : null}
     </div>
   );
 }
