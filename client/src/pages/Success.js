c1c22731aed4b3f511a7127e9be46feeb033c1c2
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-03 20:35:47 -0400

    updated success.js with REDIRECT_DELAY

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,34 +1,29 @@
-import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
-import Jumbotron from '../components/Jumbotron';
-import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+import React, { useEffect } from "react";
+import { useMutation } from "@apollo/client";
+import Jumbotron from "../components/Jumbotron";
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
+const REDIRECT_DELAY = 3000;
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
-
   useEffect(() => {
     async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
+      const cart = await idbPromise("cart", "get");
       const products = cart.map((item) => item._id);
-
       if (products.length) {
         const { data } = await addOrder({ variables: { products } });
-        const productData = data.addOrder.products;
-
+        const { products: productData } = data.addOrder; // Destructure directly
         productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
+          idbPromise("cart", "delete", item);
         });
       }
-
       setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
+        window.location.assign("/");
+      }, REDIRECT_DELAY);
     }
-
     saveOrder();
   }, [addOrder]);
-
   return (
     <div>
       <Jumbotron>
